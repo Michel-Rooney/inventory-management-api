ea3590be2e1a0bcd4daa81c32713c6789c86f0b1
Michel-Rooney <michelrooney16@gmail.com>
2024-01-25 10:59:13 -0300

    feat: Adding external database option

--- core/settings.py
+++ core/settings.py
@@ -86,12 +86,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ALLOWED_DATABASE = True if config('ALLOWED_DATABASE', '') == '1' else False
+
+if not DEBUG and ALLOWED_DATABASE:
+    DATABASES = {
+        'default': {
+            'ENGINE': config('ENGINE_DB', ''),
+            'NAME': config('NAME_DB', ''),
+            'USER': config('USER_DB', ''),
+            'PASSWORD': config('PASSWORD_DB', ''),
+            'HOST': config('HOST_DB', ''),
+            'PORT': config('PORT_DB', ''),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
